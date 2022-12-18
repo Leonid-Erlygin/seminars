db0323103d2cd8b69e6531eb6f31efd44d1d0208
Your Name <you@example.com>
2022-12-18 12:42:37 +0300

    start to write fine tune script

---
+++ s12_self-supervised/notebooks/utils.py
@@ -0,0 +1,77 @@
+import torch
+
+
+def evaluate_classifier(model, linear_classifier, loader, device):
+    correct1, correct5 = 0, 0
+    with torch.no_grad():
+        for images, target in loader:
+            images = [x.to(device) for x in images]
+            target = target.cuda(non_blocking=True)
+            out, zs = model(images)
+
+            logits = linear_classifier(zs)
+            acc1, acc5 = accuracy(logits, target, topk=(1, 5))
+
+            correct1 += acc1.item() * logits.shape[0]
+            correct5 += acc5.item() * logits.shape[0]
+    total = len(loader.dataset)
+
+    return correct1 / total, correct5 / total
+
+
+def accuracy(output, target, topk=(1,)):
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self, name, fmt=":f"):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = "{name} {val" + self.fmt + "} ({avg" + self.fmt + "})"
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print("\t".join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = "{:" + str(num_digits) + "d}"
+        return "[" + fmt + "/" + fmt.format(num_batches) + "]"
