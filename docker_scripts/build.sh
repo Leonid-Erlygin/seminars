b7145c8e689ac60c24652e8486b50f4a020cc80f
Leonid <erlygin.la@phystech.edu>
2024-09-08 18:07:28 +0000

    seminar

--- docker_scripts/build.sh
+++ docker_scripts/build.sh
@@ -1,1 +1,1 @@
-docker build -t vision_seminars .
+docker build --build-arg="GID=${UID}" --build-arg="UID=${UID}" --build-arg="NAME=${USER}" -t ${USER}_$(basename $(dirname "$PWD")) ../docker_scripts
--- docker_scripts/launch_container.sh
+++ docker_scripts/launch_container.sh
@@ -1,1 +1,1 @@
-docker run -d --shm-size=8g --memory=80g --cpus=40 --user 1005:1005 --name erlygin_seminars --rm -it --init -v /home/l.erlygin/seminars:/app --gpus '"device=0,2"' face-eval bash
+docker run -d --shm-size=8g --memory=80g --cpus=40 --gpus '"device=2"' --user ${UID}:${UID} --name ${USER}_$(basename $(dirname "$PWD"))_dev_cont --rm -it --init -v $(dirname "$PWD"):/app ${USER}_$(basename $(dirname "$PWD")) bash
