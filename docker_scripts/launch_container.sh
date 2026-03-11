1c9b9b9f0960774548d9b90195ec44d412596c62
Leonid <erlygin.la@phystech.edu>
2026-03-11 13:13:23 +0000

    upd uncertainty seminar

--- docker_scripts/launch_container.sh
+++ docker_scripts/launch_container.sh
@@ -1,1 +1,1 @@
-docker run -d --shm-size=8g --memory=80g --cpus=40 --gpus '"device=2"' --user ${UID}:${UID} --name ${USER}_$(basename $(dirname "$PWD"))_dev_cont --rm -it --init -v $(dirname "$PWD"):/app ${USER}_$(basename $(dirname "$PWD")) bash
+docker run -d --shm-size=8g --memory=80g --cpus=40 --gpus '"device=4"' --name ${USER}_$(basename $(dirname "$PWD"))_dev_cont --rm -it --init -v $(dirname "$PWD"):/app ${USER}_$(basename $(dirname "$PWD")) bash
